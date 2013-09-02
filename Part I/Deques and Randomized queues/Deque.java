e3ff1a081d30bceefe3bcc27e81bc9f96a5cb7b2
Alexey <firsaalex@gmail.com>
2013-09-03 01:27:14 +0400

    iterator problem fixed

--- Part I/Deques and Randomized queues/Deque.java
+++ Part I/Deques and Randomized queues/Deque.java
@@ -3,91 +3,111 @@ import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
     
-    private int N;        // size of the deque
-    private Node first;   // top of à he deque
-    private Node last;    // bottom of à he deque
-    
-    //helper linked list class
+    private int N;
+    private Node first, last;
     
     private class Node {
+        
         private Item item;
         private Node next;
-        private Node prev;
     }
     
-   // empty deque constructor 
+    // construct an empty deque    
     public Deque() {
         
+        N = 0;
         first = null;
         last = null;
-        N = 0;
-        assert check();
     }
     
-   // is the deque empty? 
+    // is the deque empty?
     public boolean isEmpty() {
         
-        return first == null;
+        return (first == null);
     }
-   
+    
     // return the number of items on the deque
     public int size() {
         return N;
     }
     
-    // insert the item at the front 
+
+    
+    // insert the item at the front
     public void addFirst(Item item) {
         
-        Node oldFirst = first;
+        Node oldFirst  = first;
         first = new Node();
+        first.next = oldFirst;
         first.item = item;
-        first.next = first;
-        first.prev = null;
         N++;
-        assert check();
+        
+        if (N == 1) {
+            last = first;
+        }
     }
     
-   // insert the item at the end 
+    // insert the item at the end
     public void addLast(Item item) {
         
         Node oldLast = last;
-        last = new Node();
+        last = new Node(); 
+        
+        if (N != 0) {
+            oldLast.next = last;            
+        }
+        
         last.item = item;
-        last.next = null;
-        last.prev = oldLast;
-        oldLast.next = last;
         N++;
-        assert check();
+        
+        if (N == 1) {
+            first = last;
+            last.next = null;
+        }
+        
+
     }
     
-    // delete and return the item at the front 
+    // delete and return the item at the front
     public Item removeFirst() {
-        
-        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
         Item item = first.item;
-        first = first.next;        
-        first.prev = null;
+        first = first.next;
         N--;
-        assert check();
         
         return item;
     }
     
-   // delete and return the item at the end
-    public Item removeLast() {
+    // delete and return the item at the end
+    //public Item removeLast() {
         
-        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
-        Item item = last.item;
-        last = last.prev;
-        last.next = null;
-        N--;
-        assert check();
-        
-        return item;
+    
+    //}
+    
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return new ListIterator();        
     }
     
-    private boolean check() {
-        return true;
+    private class ListIterator implements Iterator<Item> {
+        
+        private Node current = first;
+        
+        public boolean hasNext() { 
+            return (current != null);      
+        } 
+        
+        public void remove() { 
+            
+          throw new UnsupportedOperationException();  
+        } 
+        public Item next() {
+            
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        
     }
     
     public String toString() {
@@ -97,37 +117,22 @@ public class Deque<Item> implements Iterable<Item> {
         return s.toString();
     }
     
-   // return an iterator over items in order from front to end 
-    
-   
-   public Iterator<Item> iterator()  { return new ListIterator();  }
+    public static void main(String[] args) {
+        
+        Deque<String> d = new Deque<String>();
+        
+        
+        d.addFirst("1");
+        StdOut.println(d.toString());
+        d.addFirst("2");
+        StdOut.println(d.toString());
+        d.addFirst("3");
+        StdOut.println(d.toString());
+        d.addLast("4");
+        StdOut.println(d.toString());
+        d.addLast("5");
+        StdOut.println(d.toString());
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
+        
     }
-       
-
-       
-       
-    public static void main (String[] args) {
-           
-           Deque<String> s = new Deque<String>();
-           
-           s.addFirst("1");
-           
-          StdOut.println("===[" + s.toString() + "]===");
-           
-    }
-       
-       
 }
